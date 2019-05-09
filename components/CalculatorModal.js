4b5dec6cae38bb8d31f55c320957deab66608471
Ansel Bridgewater <iamansel@gmail.com>
2019-05-09 14:37:25 -0700

    Changed styles of modal

--- components/CalculatorModal.js
+++ components/CalculatorModal.js
@@ -8,6 +8,21 @@ import {
 } from 'react-native';
 
 export default class CalculatorButton extends React.Component {
+  renderCalcValues() {
+    return this.props.calcNums.map((calcNum, key) => {
+      return (
+        <TouchableHighlight
+          key={key}
+          style={styles.button}
+          onPress={() => {
+            this.props.hideModal()
+          }}>
+          <Text>{calcNum}</Text>
+        </TouchableHighlight>
+      )
+    })
+  }
+
   render() {
     return (
       <Modal
@@ -17,17 +32,18 @@ export default class CalculatorButton extends React.Component {
         onRequestClose={() => {
           Alert.alert('Modal has been closed.');
         }}>
-        <View style={{marginTop: 22}}>
+        <View style={styles.container}>
           <View>
-            <Text>Hello World!</Text>
-            {}
-            <Text>{this.props.calcNums}</Text>
+            <Text style={styles.header}>Select a value to use in this calculator</Text>
+
+            {this.renderCalcValues()}
 
             <TouchableHighlight
+              style={styles.button}
               onPress={() => {
                 this.props.hideModal()
               }}>
-              <Text>Hide Modal</Text>
+              <Text>Hide Values</Text>
             </TouchableHighlight>
           </View>
         </View>
@@ -35,3 +51,23 @@ export default class CalculatorButton extends React.Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 22,
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    margin: 10
+  },
+  header: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+});
